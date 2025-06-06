94f7cc0be4a860e44d60e7c466528c5d441e414f
Harshil Khandelwal <harshilkhandelwal21@gmail.com>
2025-06-06 22:15:02 +0530

    Minor edits

--- bot.py
+++ bot.py
@@ -2,6 +2,16 @@ import discord
 from discord.ext import commands
 import os
 from db.database import setup_db
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_WALLET_ADDRESS = os.getenv("BOT_WALLET_ADDRESS")
+BOT_WALLET_PRIVATE_KEY = os.getenv("BOT_WALLET_PRIVATE_KEY")
+WEB3_PROVIDER = os.getenv("WEB3_PROVIDER")
+
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="!", intents=intents)
